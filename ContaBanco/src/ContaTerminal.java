893469be49cfda3414fa3758779434ee9becf3c1
Arthenyo <88592884+Arthenyo@users.noreply.github.com>
2024-07-09 13:04:06 -0300

    projeto ContaBanco finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        int numero = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Por favor, digite a Agência: ");
+        String agencia = sc.nextLine();
+
+        System.out.print("Por favor, digite seu nome: ");
+        String nomeCliente = sc.nextLine();
+
+        System.out.print("Por favor, digite o valo para deposito: ");
+        double saldo = sc.nextDouble();
+
+        System.out.printf(
+                "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque",
+                nomeCliente,
+                agencia, numero, saldo);
+
+        sc.close();
     }
 }
